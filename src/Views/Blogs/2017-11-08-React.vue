95fe848175cf4fd6c686dc79e1918e5e7f7bf6c3
nykee <nykee@sina.com>
2018-04-03 15:00:10 +0800

    add d3 learning

--- src/Views/Blogs/2017-11-08-React.vue
+++ src/Views/Blogs/2017-11-08-React.vue
@@ -5,17 +5,17 @@
     </ul>
     <div class="main-body">
       <div id="part3" class="part">
-        <div class="title-lv1">一、React-router</div>
+        <div class="title-lv1">1、React-router</div>
         <p class="descrp">React-router必须设置history={browserHistory}，否则路由无法显示</p>
         <img src="../../asserts/img/1510132269(1).jpg" alt="">
 
 
-        <div class="title-lv1">React图片引入</div>
+        <div class="title-lv1">2、React图片引入</div>
         <p class="descrp">React图片地址由父组件通过props的方式传递给子组件时，必须使用字符串拼接的方式，否则图片无法加载</p>
         <pre class="code-text">
           <code>
-            父组件：<SwiperItem imgSrc="food" itemName="美食"></SwiperItem>
-            子组件：<div style={container}><img  style={imgStyle} src={require("../../images/swiper/"+this.props.imgSrc+".jpg")} alt=""/></div>
+            父组件：<span><</span>SwiperItem imgSrc="food" itemName="美食"><span><</span>/SwiperItem>
+            子组件：<span><</span>div ><span><</span>img   src={require("../../images/swiper/"+this.props.imgSrc+".jpg")} alt=""/><span><</span>/div>
           </code>
         </pre>
 
